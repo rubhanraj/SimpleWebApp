78ad7d05f0e6b19b37c8dcde4a4207b7f64430c0
Rubhan Antony Xavier Raj <rubhan.raj@qomplx.com>
2021-03-15 20:33:07 +0530

    First commit after adding javier

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -3,10 +3,15 @@ package com.develogical.app;
 public class QueryProcessor {
     public String process(String query) {
         if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+            return "R Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
+        } else if (query.toLowerCase().contains("javier")) {
+            return "Javier is an " +
+                    "Spanish poet, playwright, and actor, widely regarded as the greatest " +
+                    "writer in the Spanish language and the world's pre-eminent dramatist.";
         }
         return "";
+
     }
 }
